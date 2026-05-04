a5aee853d3bdede0947ef0f27426e744c20adbce
Antonio Suarez <jasmantonio.suarez@gmail.com>
2026-05-04 15:36:00 -0600

    Ejercicio 1 commit

---
+++ tipos-de-datos.js
@@ -0,0 +1,14 @@
+console.log("Tipos de datos en JavaScript:");
+
+console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof "''");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof FALSE);
+console.log(typeof "Hola Mundo!");
+console.log(typeof 3.1416);
+console.log(typeof NaN);
